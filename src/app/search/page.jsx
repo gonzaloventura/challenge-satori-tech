79d48e94336e45442fdc0510bc529374d18a7422
Gonzalo Ventura <gonzalogventura@gmail.com>
2023-11-17 16:37:57 -0300

    Si no hay resultados no muestra nada

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -11,6 +11,19 @@ export default async function SearchPage({ searchParams }) {
     const params = searchParams
     const search = await obtenerLocations(params.location)
 
+    if (params.location == 'undefined') {
+        return (
+            <div className="flex flex-col gap-10 p-4 md:p-10">
+                {/* <h1 className="text-lg md:text-2xl font-semibold">{search.length == (null || undefined) ? 'No se encontraron resultados con "' + params.q + '"' : (search.length === 1 ? 'Se encontró 1 resultado con "' + params.q + '"' : "Se encontraron " + search.length + ' resultados con "' + params.q + '"')}</h1> */}
+                <main>
+                    <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5 lg:gap-6 md:gap-8 grid-flow-row  items-start">
+                        <p>No se encontraron resultados</p>
+                    </div>
+                </main>
+            </div>
+        )
+    }
+
     return (
         <div className="flex flex-col gap-10 p-4 md:p-10">
             {/* <h1 className="text-lg md:text-2xl font-semibold">{search.length == (null || undefined) ? 'No se encontraron resultados con "' + params.q + '"' : (search.length === 1 ? 'Se encontró 1 resultado con "' + params.q + '"' : "Se encontraron " + search.length + ' resultados con "' + params.q + '"')}</h1> */}
@@ -18,7 +31,7 @@ export default async function SearchPage({ searchParams }) {
                 <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5 lg:gap-6 md:gap-8 grid-flow-row  items-start">
                     {search.residents.slice(0, 5).map((item) => {
                         return (
-                            <CardsContainer key={item.id} data={item} background={params.location} /> 
+                            <CardsContainer key={item.id} data={item} background={params.location} />
                         )
                     })
                     }
